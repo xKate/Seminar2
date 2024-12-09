dc24819dba6f1168795a6a27560e56c3ea2527d3
Kate Filipovich <xkoshka@mail.ru>
2024-12-09 21:29:50 +0300

    Задача 2.Создание метода printEvenNums

---
+++ Answer1.java
@@ -0,0 +1,16 @@
+// Напишите метод printEvenNums, который выведет на экран все четные числа
+//в промежутке от 1 до 100, каждое на новой строке
+public class Answer1 {
+    
+    public static void printEvenNums() {
+        // Напишите свое решение н
+        for ( int i = 1; i <= 100; i++) {
+            if (i % 2 == 0) {
+                System.out.println(i);       
+            }
+        
+        }   
+    }
+}    
+       
+
