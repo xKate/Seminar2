cf827e6f8e384ae21ba657f681d9ecaae5240ed7
Kate Filipovich <xkoshka@mail.ru>
2024-12-09 19:00:31 +0300

    Задача 1. Написан метод факториал

---
+++ Answer.java
@@ -0,0 +1,13 @@
+class Answer {
+    public int factorial(int n) {
+    // Введите свое решение ниже
+        int c = 1;
+        for (int i = 1; i <= n; i++){
+            c = c * i;
+        
+        }
+        return c;
+    }
+}  
+    
+
